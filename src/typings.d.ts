f9a84975aac5f52bc6cd28155f5c559b12dba7f1
Amit Portnoy <1131991+amitport@users.noreply.github.com>
2018-08-02 18:50:01 +0300

    avoid duplicated boardgame.io client

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,1 +1,20 @@
 declare module 'boardgame.io*';
+
+declare module 'boardgame.io/dist/client' {
+  export class Client {
+    moves: {[moveName: string]: (...args) => void};
+    events: {[eventName: string]: (...args) => void};
+
+    constructor(opt: any);
+
+    getState(): any;
+    reset(): void;
+    undo(): void;
+    redo(): void;
+    step(): void;
+    connect(): void;
+    updateGameID(gameID: string): void;
+    updatePlayerID(playerID: string): void;
+    updateCredentials(credentials: string): void;
+  }
+}

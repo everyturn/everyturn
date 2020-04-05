70d72aa4bb8222c50d69c9603f61fae0be9ad3a4
Amit Portnoy <1131991+amitport@users.noreply.github.com>
2020-04-05 16:18:51 +0300

    simplify demo app

--- packages/webapp/src/game-component.ts
+++ packages/webapp/src/game-component.ts
@@ -1,8 +1,14 @@
 import { css, customElement, html, LitElement, property } from 'lit-element';
 // remove /lib/ on https://github.com/rollup/plugins/issues/208
-import { cards } from '@everyturn/core/lib/cards.js';
+import { Card, cards } from '@everyturn/core/lib/cards.js';
 
-const cardElements = cards.map(({rank, suit}) => html`<div>${rank}${suit}</div>`);
+const suit2color = {
+  'C': 'black', 'D': 'red', 'H': 'red', 'S': 'black'
+};
+const suit2symbol = {
+  'C': '♣', 'D': '♦', 'H': '♥', 'S': '♠'
+};
+const cardTemplate = ({ rank, suit}: Card) => html`<div class="card card--color-${suit2color[suit]}">${rank}${suit2symbol[suit]}</div>`;
 
 @customElement('et-game')
 export class GameComponent extends LitElement {
@@ -13,6 +19,17 @@ export class GameComponent extends LitElement {
         padding: 25px;
         color: var(--everyturn-game-text-color, #000);
       }
+
+      .card {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: 1px solid gray;
+        height: 28px;
+        width: 28px;
+      }
+      .card.card--color-black {color: black;}
+      .card.card--color-red {color: red;}
     `;
   }
 
@@ -27,7 +44,7 @@ export class GameComponent extends LitElement {
     return html`
       <h2>${this.title} Nr. ${this.counter}!</h2>
       <button @click=${this.__increment}>increment</button>
-      ${cardElements}
+      ${cards.map(cardTemplate)}
     `;
   }
 }
